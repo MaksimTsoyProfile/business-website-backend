7e04da4a6ed4a029b87fc62d50b8d9e80c443a39
Maksim Tsoy <maksimtsoy1990@mail.ru>
2023-07-11 19:40:01 +0500

    add: strapi project

---
+++ config/middlewares.js
@@ -0,0 +1,20 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: '*',
+      methods: '*',
+    }
+  },
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
